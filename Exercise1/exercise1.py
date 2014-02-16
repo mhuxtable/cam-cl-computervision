c509eae7479db9ffa1f563092a9d63b481e69aef
Matthew Huxtable <mjh233@cam.ac.uk>
2014-02-16 15:17:55 +0000

    Exercise 1 completed from last week

--- Exercise1/exercise1.py
+++ Exercise1/exercise1.py
@@ -9,7 +9,6 @@ import cv2, numpy as np, os
 ## Exercise 1.1
 def basic_convolution(image, kernel, verbose=False):
   'Computes the convolution of an image with a kernel.'
-
   # Allocate memory for result.
   result = np.zeros_like(image)
   
@@ -20,9 +19,13 @@ def basic_convolution(image, kernel, verbose=False):
   # Iterate over all pixels in the image
   for i in range(mend - 1, iend):
     for j in range(nend - 1, jend):
-        ## TODO
-        ## Implement convolution here
-        result[i][j] = 0
+      sum = float(0)
+      for m in range(0, mend):
+        for n in range(0, nend):
+          sum += kernel[m][n] * image[i-m][j-n]
+
+      # kernel already normalised, so / (mend * nend) factor omitted
+      result[i][j] = sum
     if verbose: print "basic_convolution: Filtering row %3i of %i ..." % (i + 1, iend)
   
   return result
@@ -31,25 +34,49 @@ def basic_convolution(image, kernel, verbose=False):
 ## Exercise 1.2
 def padded_convolution(image, kernel, verbose=False):
   'Computes the convolution of an image with a kernel, with clamp-to-edge.'
-
+  print "padded_convolution: Computing the padded convolution of the image."
+  border=5
   ## TODO
   ## Enlarge the image so that there is information in the previously invalid areas
+  # Expand by 5 pixels in every direction. Then take the convolution, which will produce
+  # a 10 pixel wide border around the side which is removed later.
+  expanded = cv2.copyMakeBorder(image, border,border,border,border, cv2.BORDER_WRAP);  
   
   ## Perform a convolution on this image
+  convolved = basic_convolution(expanded, kernel, verbose)
   
   ## Return the centred result
-  
-  return np.zeros(image.shape, 'float')
+  (x,y) = convolved.shape
+  # Cut off the 10 pixel wide black edge from the return of the convolution
+  # (10x10 as the kernel is 10x10, and we don't convolve at the edge as (0,0)
+  # is not the centre of the kernel but the corner)
+  return convolved[border*2:, border*2:]
 
 
 ## Exercise 1.3
 def basic_convolution_dft(image, kernel, verbose=False):
   'Computes the convolution of an image with a kernel using a basic DFT-based approach.'
    
+  print "basic_convolution_dft: Computing the DFT-based convolution of the image."
+
   output = np.zeros(image.shape, 'float')
-  
+
+  kernelpad = np.zeros_like(image)
+  (x,y) = kernel.shape
+  kernelpad[:x,:y] = kernel
   ## TODO
   ## Implementation of convolution theorem
+  imagefft = np.fft.fft2(image)
+  kernelfft = np.fft.fft2(kernelpad)
+
+  resultfft = np.zeros_like(imagefft)
+  (iend, jend) = imagefft.shape
+  (mend, nend) = kernelfft.shape
+  
+  resultfft = np.multiply(imagefft, kernelfft)
+  
+  output = np.fft.ifft2(resultfft)
+  output = np.absolute(output)
   
   ## Return the result.
   if output.dtype == np.complex128:
@@ -60,12 +87,28 @@ def basic_convolution_dft(image, kernel, verbose=False):
 ## Exercise 1.4
 def padded_convolution_dft(image, kernel, verbose=False):
   'Computes the convolution of an image with a kernel using a DFT-based approach (clamp-to-edge).'
-    
-  output = np.zeros(image.shape, 'float')
   
+  print "padded_convolution_dft: Computing the padded DFT-based convolution of the image."
+
   ## TODO
   ## Implementation of convolution theorem with padding
+  border=5
+  ## TODO
+  ## Enlarge the image so that there is information in the previously invalid areas
+  # Expand by 5 pixels in every direction. Then take the convolution, which will produce
+  # a 10 pixel wide border around the side which is removed later.
+  expanded = cv2.copyMakeBorder(image, border,border,border,border, cv2.BORDER_WRAP);  
   
+  ## Perform a convolution on this image
+  convolved = basic_convolution_dft(expanded, kernel, False)
+  
+  ## Return the centred result
+  (x,y) = convolved.shape
+  # Cut off the 10 pixel wide black edge from the return of the convolution
+  # (10x10 as the kernel is 10x10, and we don't convolve at the edge as (0,0)
+  # is not the centre of the kernel but the corner)
+  output = convolved[border*2:, border*2:]
+
   ## Return the result.
   if output.dtype == np.complex128:
     raise Exception("Output image is complex valued.")
@@ -129,14 +172,17 @@ if __name__ == '__main__':
   if verbose: print "kernel = %i x %i" % kernel.shape
   
   result1 = basic_convolution(image, kernel, verbose=verbose)
+  #result1 = np.zeros_like(image)
   if verbose: show("Result 1 (basic)", result1)
   result2 = padded_convolution(image, kernel, verbose=verbose)
+  #result2 = np.zeros_like(image)
+
   if verbose: show("Result 2 (padded)", result2)
   result3 = basic_convolution_dft(image, kernel, verbose=verbose)
   if verbose: show("Result 3 (basic DFT)", result3)
   result4 = padded_convolution_dft(image, kernel, verbose=verbose)
   if verbose: show("Result 4 (padded DFT)", result4)
-  
+
   ## Save images to disk for comparison.
   cv2.imwrite(os.path.expanduser("image-input.png"), np.uint8(255 * image))
   cv2.imwrite(os.path.expanduser("image-basic_convolution.png"), np.uint8(255 * result1))
@@ -165,7 +211,7 @@ if __name__ == '__main__':
       print "    Result is all zeros."
     if result2.shape != result2_correct.shape:
       print "    The size is incorrect. The result should be the same size as the input"
-      
+
   result3_correct = cv2.imread(os.path.expanduser("answer-images/image-basic_convolution-dft.png"), 0) / 255.0
   if are_similar(result3, result3_correct):
     print "Exercise 1.3: CORRECT"
